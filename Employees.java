e3d734f6311c986f6d0c37f0d897ea82bc5a639c
Vansh Jagetia <vanshjagetia3608@gmail.com>
2026-02-17 21:08:57 +0530

    Refactor Employees class and update methods

--- Employees.java
+++ Employees.java
@@ -1,38 +1,40 @@
-import java.util.Scanner;
-import java.util.UUID;
-public class Employees extends Showroom implements utility{
+import java.util.*;
+
+public class Employees implements utility{
     String emp_id;
     String emp_name;
     int emp_age;
     String emp_department;
-
+    String assigned_showroom;
 
     @Override
-    public void get_details() {
-        System.out.println("ID: "+emp_id);
-        System.out.println("Name: "+emp_name);
-        System.out.println("Age: "+emp_age);
-        System.out.println("Department: "+emp_department);
-        System.out.println("Showroom Name: "+showroom_name);
-
+    public void get_details(){
+        System.out.println("EMPLOYEE ID: "+emp_id);
+        System.out.println("NAME: "+emp_name);
+        System.out.println("AGE: "+emp_age);
+        System.out.println("DEPARTMENT: "+emp_department);
+        System.out.println("SHOWROOM: "+assigned_showroom);
+        System.out.println("--------------------------------");
     }
+
     @Override
-    public void set_details(){
-        Scanner sc = new Scanner(System.in);
-        UUID uuid = UUID.randomUUID();
-        emp_id = String.valueOf(uuid);
-        System.out.println("======================= *** ENTER EMPLOYEE DETAILS *** =======================");
-        System.out.println();
-        System.out.print("EMPLOYEE NAME: ");
-        emp_name = sc.nextLine();
-        System.out.print(("EMPLOYEE AGE: "));
-        emp_age = sc.nextInt();
+    public void set_details(Scanner sc){
+        emp_id=UUID.randomUUID().toString();
+        System.out.println("ENTER EMPLOYEE DETAILS");
+        System.out.print("NAME: ");
+        emp_name=sc.nextLine();
+        do{
+            System.out.print("AGE: ");
+            emp_age=sc.nextInt();
+        }while(emp_age<=18);
         sc.nextLine();
-        System.out.print("EMPLOYEE DEPARTMENT: ");
-        emp_department = sc.nextLine();
-        System.out.print("SHOWROOM NAME: ");
-        showroom_name = sc.nextLine();
+        System.out.print("DEPARTMENT: ");
+        emp_department=sc.nextLine();
+        System.out.print("ASSIGNED SHOWROOM: ");
+        assigned_showroom=sc.nextLine();
+    }
 
-
+    public void updateDepartment(String dept){
+        emp_department=dept;
     }
 }
