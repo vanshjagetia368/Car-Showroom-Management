5559d0889275ab439da7a2afb6cb3b8a6b8abbc4
Vansh Jagetia <vanshjagetia3608@gmail.com>
2026-02-17 21:09:24 +0530

    Refactor Showroom class to manage employees and cars

--- Showroom.java
+++ Showroom.java
@@ -1,35 +1,79 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Showroom implements utility{
+    String showroom_id;
     String showroom_name;
     String showroom_address;
-    int total_employees;
-    int total_cars_in_stock = 0;
     String manager_name;
+    int total_employees=0;
+    int total_cars_in_stock=0;
+    double total_revenue=0;
+
+    ArrayList<Employees> employees=new ArrayList<>();
+    ArrayList<Cars> cars=new ArrayList<>();
 
     @Override
-    public void get_details() {
-        System.out.println("Showroom Name: "+showroom_name);
-        System.out.println("Showroom Address: "+showroom_address);
-        System.out.println("Manager Name: "+manager_name);
-        System.out.println("Total Employees: "+total_employees);
-        System.out.println("Total Cars In Stock: "+total_cars_in_stock);
-
+    public void get_details(){
+        System.out.println("SHOWROOM ID: "+showroom_id);
+        System.out.println("NAME: "+showroom_name);
+        System.out.println("ADDRESS: "+showroom_address);
+        System.out.println("MANAGER: "+manager_name);
+        System.out.println("TOTAL EMPLOYEES: "+total_employees);
+        System.out.println("TOTAL CARS: "+total_cars_in_stock);
+        System.out.println("TOTAL REVENUE: "+total_revenue);
     }
-    @Override
-    public void set_details(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("======================= *** ENTER SHOWROOM DETAILS *** =======================");
-        System.out.println();
-        System.out.print("SHOWROOM NAME: ");
-        showroom_name = sc.nextLine();
-        System.out.print(("SHOWROOM ADDRESS: "));
-        showroom_address = sc.nextLine();
-        System.out.print("MANAGER NAME: ");
-        manager_name = sc.nextLine();
-        System.out.print("TOTAL NO OF EMPLOYEES: ");
-        total_employees = sc.nextInt();
-        System.out.print("TOTAL CARS IN STOCK: ");
-        total_cars_in_stock = sc.nextInt();
 
+    @Override
+    public void set_details(Scanner sc){
+        showroom_id=UUID.randomUUID().toString();
+        System.out.println("ENTER SHOWROOM DETAILS");
+        System.out.print("SHOWROOM NAME: ");
+        showroom_name=sc.nextLine();
+        System.out.print("SHOWROOM ADDRESS: ");
+        showroom_address=sc.nextLine();
+        System.out.print("MANAGER NAME: ");
+        manager_name=sc.nextLine();
+    }
+
+    public void addEmployee(Employees e){
+        employees.add(e);
+        total_employees++;
+    }
+
+    public void removeEmployee(String id){
+        Iterator<Employees> it=employees.iterator();
+        while(it.hasNext()){
+            if(it.next().emp_id.equals(id)){
+                it.remove();
+                total_employees--;
+                break;
+            }
+        }
+    }
+
+    public void addCar(Cars c){
+        cars.add(c);
+        total_cars_in_stock++;
+    }
+
+    public void sellCar(String id){
+        Iterator<Cars> it=cars.iterator();
+        while(it.hasNext()){
+            Cars c=it.next();
+            if(c.car_id.equals(id)){
+                total_revenue+=c.car_price;
+                it.remove();
+                total_cars_in_stock--;
+                break;
+            }
+        }
+    }
+
+    public void showEmployees(){
+        for(Employees e:employees) e.get_details();
+    }
+
+    public void showCars(){
+        for(Cars c:cars) c.get_details();
     }
 }
